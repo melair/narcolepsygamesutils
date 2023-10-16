02087daf0707eb53169b396114c29768f4948411
Melair <melair@caldonia.net>
2023-10-16 12:08:57 +0100

    Heal players one heart every time they sleep.

---
+++ src/main/java/games/narcolepsy/minecraft/utils/features/villagerheal/VillagerHeal.java
@@ -0,0 +1,42 @@
+package games.narcolepsy.minecraft.utils.features.villagerheal;
+
+import games.narcolepsy.minecraft.utils.features.BaseFeature;
+import org.bukkit.Particle;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Damageable;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Pose;
+import org.bukkit.entity.Villager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPoseChangeEvent;
+import org.bukkit.plugin.Plugin;
+
+public class VillagerHeal extends BaseFeature implements Listener {
+    public VillagerHeal(Plugin plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public void Enable() {
+        this.server.getPluginManager().registerEvents(this, this.plugin);
+    }
+
+    @Override
+    public String getName() {
+        return "Villager Heal";
+    }
+
+    @EventHandler
+    public void onEntityPoseChangeEvent(EntityPoseChangeEvent e) {
+        if (e.getEntity() instanceof Villager v) {
+            if (e.getPose() == Pose.SLEEPING) {
+                var maxHealth = v.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+                if (v.getHealth() < maxHealth) {
+                    v.setHealth(Math.min(maxHealth, v.getHealth() + 2));
+                    v.getWorld().spawnParticle(Particle.HEART, e.getEntity().getLocation(), 1, 0.25, 0.25, 0.25);
+                }
+            }
+        }
+    }
+}
